281c15362829b9e4a109454199ee1eb31c15f380
Samir Shaikh <samirshaikh.co.in@gmail.com>
2023-12-28 22:02:07 +0530

    ..

--- src/pages/components/Dashboard/index.tsx
+++ src/pages/components/Dashboard/index.tsx
@@ -1,12 +1,13 @@
 import { useEffect } from "react";
 
 export default function DashboardPage({ posts }:any) {
-  
-  console.log(posts);
+
+  console.log(posts, "---");
   return <h2>hello dashboard</h2>;
 }
 
 export async function getServerSideProps() {
+  console.log("---, FROM");
   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
   const posts = await res.json();
 
