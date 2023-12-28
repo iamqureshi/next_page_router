c7fd180bd6cc2d90f2d713301bd348ea96c4e2df
Samir Shaikh <samirshaikh.co.in@gmail.com>
2023-12-28 21:42:05 +0530

    add minor changes

--- src/pages/components/Dashboard/index.tsx
+++ src/pages/components/Dashboard/index.tsx
@@ -1,15 +1,15 @@
-export default function DashboardPage({ posts }: any) {
-  console.log("hell", posts)
+export default function DashboardPage(props: { posts: any }) {
+  console.log("from Dashboard Page", props.posts);
+
   return <h2>hello dashboard</h2>;
 }
 
-
 export async function getStaticProps() {
-  console.log("hell-1")
+  console.log("from getStaticProps from dashboard page");
 
-  const res = await fetch("https://jsonplaceholder.typicode.com/todos/1")
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
   const posts = await res.json();
-  
+
   return {
     props: {
       posts,
