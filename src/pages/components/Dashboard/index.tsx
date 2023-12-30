82a965baf4d36fa06126d9aaea5add21f07814a5
Samir Shaikh <samirshaikh.co.in@gmail.com>
2023-12-30 18:08:50 +0530

    add getStaticProps

--- src/pages/components/Dashboard/index.tsx
+++ src/pages/components/Dashboard/index.tsx
@@ -1,16 +1,7 @@
-export default function DashboardPage({ posts }: any) {
-  console.log(posts, "---");
-  return <h2>hello dashboard</h2>;
-}
-
-export async function getServerSideProps() {
-  console.log("---, FROM");
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const posts = await res.json();
-
-  return {
-    props: {
-      posts,
-    },
-  };
+export default function DashboardPage({ repo }: any) {
+  return (
+    <>
+      <h1>This is Data {JSON.stringify(repo)}</h1>
+    </>
+  );
 }
